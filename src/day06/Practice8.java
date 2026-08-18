f9bb5d4268aec44f47a5ac376e6018d9f8c0ca53
jaeui777 <jaeui777@gmail.com>
2026-08-18 11:41:59 +0900

    Update 0818 3H

---
+++ src/day06/Practice8.java
@@ -0,0 +1,96 @@
+package day06;
+
+
+public class Practice8 {
+    public static void main(String[] args) {
+        // [1]
+        Book b1 = new Book("이것이 자바다", "신용권", 30000 );
+        Book b2 = new Book("자바의 정석", "남궁성", 28000 );
+
+        // [2]
+        Rectangle r1 = new Rectangle( 10, 0);
+
+        // [3] 기본생성자 사용하는 경우 vs 정의생성자 사용하는 경우
+        BankAccount account = 
+            new BankAccount("111-222-3333", "유재석", 10000);
+
+
+        // [4]
+        Goods g1 = new Goods();
+        Goods g2 = new Goods("콜라", 2000);
+
+        System.out.println(g1.name + ", " + g1.price);
+        System.out.println(g2.name + ", " + g2.price);  
+
+        // [5]
+        Member m1 = new Member();
+
+        System.out.println(m1.id);
+        System.out.println(m1.isLogin);
+
+    } // main end
+} // class end
+
+class BankAccount{ // 클래스란? 객체(인스턴스) 만들기 위한 설계도( 실체가 아니다. )
+    // * 클래스(설계도)가 동일하더라도 객체(인스턴스)는 new 마다 서로다른 실체!
+    String accountNumber; // 멤버 변수(필드) : 객체가 생성되면 객체내 사용 할 변수 뜻
+    String ownerName;
+    int balance;
+    // 생성자란? 객체(인스턴스) 생성할 때 초기값 대입
+    // 매개변수? 외부로부터 생성자/메소드에게 전달되는 값(인수) 저장하는 변수
+    // 관례적으로 생성자의 매개변수는 멤버변수명과 동일하며 this 구분
+    BankAccount( String accountNumber, String ownerName, int balance ){
+        this.accountNumber = accountNumber; // this.멤버변수명 = 매개변수명
+        this.ownerName = ownerName;
+        this.balance = balance;
+    }
+}
+
+class Rectangle{
+    double width;
+    double height;
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+}
+
+class Book{
+    // 1.멤버변수 
+    String title;
+    String author;
+    int price;
+    // 2. 생성자 : 
+    // *자동: 클래스내 빈공간 오른쪽 -> 소스 작업 
+    //        -> constructor -> 멤버변수 체크 -> [확인]*
+    Book( String title , String author , int price ){
+        this.title = title;
+        // this.멤버변수명 = 매개변수명;
+        this.author = author;
+        this.price = price;
+    }
+    // 3. 메소드 
+}
+
+
+class Goods {
+    String name;
+    int price;
+    Goods() {name = "미정"; price = 0;}
+    Goods(String name, int price) {this.name = name; this.price = price;}
+}
+
+
+class Member {
+    String id;
+    boolean isLogin;
+    Member() {id = "guest"; isLogin = false;}
+}
+ 
+
+
+/*[문제 5] Member 클래스 (기본 생성자 및 초기값 설정)
+Member 클래스를 정의하세요. (멤버 변수: id, isLogin)
+멤버 변수의 타입은 적절하게 선택하시오.
+Member 클래스에 기본 생성자를 만들고, 생성자 내부에서 id는 "guest", isLogin은 false로 초기화되도록 구현하세요.
+main 함수에서 new Member()로 객체를 생성한 뒤, 초기화된 id와 isLogin 값을 출력하여 확인하세요.*/
